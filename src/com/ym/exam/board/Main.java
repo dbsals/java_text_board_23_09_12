b2ea7db648f334e34fba644dfa661fc3a50757aa
dbsals <dbsals999@naver.com>
2023-09-14 16:14:20 +0900

    명령어 입력받기

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -1,10 +1,18 @@
 package com.ym.exam.board;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
     System.out.println("== 자바 텍스트 게시판 ==");
     System.out.println("== 프로그램 시작 ==");
-    System.out.println("명령) ");
+    System.out.printf("명령) ");
+    String cmd = sc.nextLine();
+    System.out.printf("입력받은 명령어 : %s\n", cmd);
     System.out.println("== 프로그램 종료 ==");
+
+    sc.close();
   }
 }
