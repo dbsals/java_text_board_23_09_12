ffeeef61d98272f07aad650e2663af143a7869c7
dbsals <dbsals999@naver.com>
2023-09-15 01:15:17 +0900

    매개변수 있는 생성자를 추가하여, 게시물 객체를 쉽게 만들기

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -24,10 +24,7 @@ public class Main {
 
         number++;
 
-        Article articles = new Article();
-        articles.id = number;
-        articles.title = title;
-        articles.content = content;
+        Article articles = new Article(number, title, content);
 
         System.out.printf("%d번 게시물이 등록되었습니다.\n", number);
         System.out.println(articles);
@@ -51,6 +48,13 @@ class Article{
   int id;
   String title;
   String content;
+
+  Article(int id, String title, String content){
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
   @Override
   public String toString(){
     return String.format("{id : %d, title : \"%s\", content : \"%s\"}", id, title, content);
