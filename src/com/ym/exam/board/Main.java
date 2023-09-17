3f1319f31be3d6ef1bed4ad5465b0d2c8f4179ed
dbsals <dbsals999@naver.com>
2023-09-18 00:19:11 +0900

    Query - 쿼리스트링을 파싱하여 List가 아닌 Map에 넣기

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -1,30 +1,31 @@
 package com.ym.exam.board;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString = "a=10&b=20&c=30&d=40";
+    String queryString = "id=5&title=aaa&content=bbbb&writerName=홍길동";
 
-    List<String> paramNames = new ArrayList<>();
-    List<Integer> paramValues = new ArrayList<>();
+    Map<String, String> params = new HashMap<>();
 
     String[] queryStringbits = queryString.split("&");
 
     for(String bit : queryStringbits){
       String[] bitBits = bit.split("=");
-      String paramName = bitBits[0];
-      String paramValue = bitBits[1];
-
-      paramNames.add(paramName);
-      paramValues.add(Integer.parseInt(paramValue));
+      params.put(bitBits[0], bitBits[1]);
 
     }
+    System.out.println("==원하는 것 하나하나 가져와서 사용");
+    System.out.printf("id : %d\n", Integer.parseInt(params.get("id")));
+    System.out.printf("title : %s\n", params.get("title"));
+    System.out.printf("content : %s\n", params.get("content"));
+    System.out.printf("writeName : %s\n", params.get("writerName"));
 
-    for(int i = 0; i < paramNames.size(); i++){
-      System.out.printf("%s : %d\n", paramNames.get(i), paramValues.get(i));
+    System.out.println("== 반복문으로 전체 출력 ==");
+    for(String paramName : params.keySet()){
+      String paramValue = params.get(paramName);
+      System.out.printf("%s : %s\n", paramName, paramValue);
+
     }
   }
 }
