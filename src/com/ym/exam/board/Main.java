b66b6d32e9a40ff3d0f8177b51dea5b41b7ebb85
dbsals <dbsals999@naver.com>
2023-09-19 20:52:58 +0900

    Query - url로 부터 쿼리스트링 정보를 얻고, 그것을 파싱하기

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -4,26 +4,35 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    String queryString1 = "id=5&title=aaa&content=bbbb&writerName=홍길동&name=[1=2]";
-    Map<String, String> params1 = Util.getParams(queryString1);
-    System.out.println(params1);
-    System.out.printf("content : %s\n", params1.get("content"));
+    Map<String, String> params = Util.getParamForomUrl("/usr/article/list?page=2&searchKeyword=안녕");
+
+    System.out.println(params);
+    System.out.println(params.get("page"));
+    System.out.println(params.get("searchKeyword"));
   }
 }
 
 class Util {
-  static Map<String, String> getParams(String getStr) {
-    Map<String, String> params = new HashMap<>();
+  static Map<String, String> getParamForomUrl(String url) {
+    Map<String, String> Params = new HashMap<>();
 
-    for(String bit : getStr.split("&")) {
+    String[] urlBits = url.split("\\?", 2);
+
+    if(urlBits.length == 1) {
+      return Params;
+    }
+    String QueryStr = urlBits[1];
+
+    for(String bit : QueryStr.split("&")) {
       String[] bits = bit.split("=", 2);
-      if(bits.length == 1) {
+
+      if(bits.length == 1){
         continue;
       }
 
-      params.put(bits[0], bits[1]);
+      Params.put(bits[0], bits[1]);
     }
 
-    return params;
+    return Params;
   }
 }
