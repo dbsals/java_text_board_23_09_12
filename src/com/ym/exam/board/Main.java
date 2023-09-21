c9ddf64b9f51de2dc46493083437238685554f2c
dbsals <dbsals999@naver.com>
2023-09-21 19:52:24 +0900

    복잡한 명령어 처리를 위해 Rq 도입

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -1,43 +1,122 @@
 package com.ym.exam.board;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
-class Main {
+public class Main {
+
+  static void makeTestDate(List<Article> articles) {
+    articles.add(new Article(1, "제목1", "내용1"));
+    articles.add(new Article(2, "제목2", "내용2"));
+    articles.add(new Article(3, "제목3", "내용3"));
+  }
   public static void main(String[] args) {
-    Rq rq = new Rq("/usr/article/list?page=1&searchKeyword=안녕? 어서와");
+    Scanner sc = new Scanner(System.in);
+    int articleLastId = 0;
+    List<Article> articles = new ArrayList<>();
 
-    Map<String, String> params = rq.getParams();
-    System.out.println(params); // {page=1, searchKeyword=안녕? 어서와}
-    System.out.println(rq.getParams()); // {page=1, searchKeyword=안녕? 어서와}
-    System.out.println(rq.getParams()); // {page=1, searchKeyword=안녕? 어서와}
+    makeTestDate(articles);
 
+    if(articles.size() > 0) {
+      articleLastId = articles.get(articles.size() - 1).id;
+    }
 
-    String urlPath = rq.getUrlPath();
-    System.out.println(urlPath); // /usr/article/list
-    System.out.println(rq.getUrlPath()); // /usr/article/list
-    System.out.println(rq.getUrlPath()); // /usr/article/list
+    System.out.println("== 자바 텍스트 게시판 ==");
+    System.out.println("== 프로그램 시작 ==");
+
+    while (true) {
+      System.out.printf("명령) ");
+      String cmd = sc.nextLine();
+
+      Rq rq = new Rq(cmd);
+
+      if(rq.getUrlPath().equals("/usr/article/write")) {
+        System.out.println("== 게시물 등록 ==");
+        System.out.printf("제목 : ");
+        String title = sc.nextLine();
+
+        System.out.printf("내용 : ");
+        String content = sc.nextLine();
+
+        int id = articleLastId + 1;
+        articleLastId = id;
+
+        Article article = new Article(id, title, content);
+
+        articles.add(article); // list에 게시물 추가
+
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+      }
+      else if(rq.getUrlPath().equals("/usr/article/list")) {
+        System.out.println("== 게시물 리스트 ==");
+        System.out.println("-------------------");
+        System.out.println("번호 / 제목");
+        System.out.println("-------------------");
+
+        for(int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d / %s\n", article.id, article.title);
+        }
+
+      }
+      else if(rq.getUrlPath().equals("/usr/article/detail")) {
+
+        if(articles.isEmpty()) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = articles.get(articles.size() - 1);
+
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.content);
+      }
+      else if(rq.getUrlPath().equals("exit")) {
+        System.out.println("프로그램을 종료합니다.");
+        break;
+      }
+      else {
+        System.out.println("올바른 명령어를 입력해주세요.");
+      }
+    }
+
+    sc.close();
+  }
+}
+
+class Article {
+  int id;
+  String title;
+  String content;
+
+  Article(int id, String title, String content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("{id : %d, title : \"%s\", content : \"%s\"}", id, title, content);
   }
 }
 
 class Rq {
   String url;
-  // 필드추가가능
-  Map<String, String> Params;
+  Map<String, String> params;
   String urlPath;
-  // 수정불가능
+
   Rq(String url) {
     this.url = url;
-    Params = Util.getParamsFromUrl(url);
+    params = Util.getParamsFromUrl(url);
     urlPath = Util.getUrlPathFromUrl(url);
   }
 
-  // 수정가능
   public Map<String, String> getParams() {
-    return Params;
+    return params;
   }
 
-  // 수정가능
   public String getUrlPath() {
     return urlPath;
   }
@@ -45,7 +124,6 @@ class Rq {
 
 class Util {
   static Map<String, String> getParamsFromUrl(String url) {
-    System.out.println("getParamsFromUrl 실행됨");
     Map<String, String> params = new HashMap<>();
     String[] urlBits = url.split("\\?", 2);
 
@@ -69,7 +147,6 @@ class Util {
   }
 
   static String getUrlPathFromUrl(String url) {
-    System.out.println("getUrlPathFromUrl 실행됨");
     return url.split("\\?", 2)[0];
   }
 }
