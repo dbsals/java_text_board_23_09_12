4887155a9dc9f661c5af87b15980cf31c9c25481
dbsals <dbsals999@naver.com>
2023-10-05 19:24:35 +0900

    static 초기화 블록 사용

--- src/com/ym/exam/board/Main.java
+++ src/com/ym/exam/board/Main.java
@@ -5,8 +5,13 @@ import com.ym.exam.board.container.Container;
 import java.util.*;
 
 public class Main {
-  static int articleLastId = 0;
-  static List<Article> articles = new ArrayList<>();
+  static int articleLastId;
+  static List<Article> articles;
+
+  static {
+    articleLastId = 0;
+    articles = new ArrayList<>();
+  }
 
   static void makeTestDate(List<Article> articles) {
     for(int i = 1; i <= 100; i++) {
--- src/com/ym/exam/board/container/Container.java
+++ src/com/ym/exam/board/container/Container.java
@@ -3,5 +3,9 @@ package com.ym.exam.board.container;
 import java.util.Scanner;
 
 public class Container {
-  public static Scanner sc = new Scanner(System.in);
+  public static Scanner sc;
+
+  static {
+    sc = new Scanner(System.in);
+  }
 }
